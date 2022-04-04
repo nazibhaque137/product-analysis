0222da26ad2907ed52c790318758a49f7bdefd19
nazibhaque137 <nazibhaque137@gmail.com>
2022-04-04 20:25:33 +0600

    CustomLink added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink'
 
 const Header = () => {
     return (
         <div>
-     <h1>Smart Watch World Header</h1>
         <nav>
-                <Link to="/">Home</Link>
-                <Link to="/reviews">Reviews</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/blogs">Blogs</Link>
-                <Link to="/about">About</Link>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/reviews">Reviews</CustomLink>
+                <CustomLink to="/dashboard">Dashboard</CustomLink>
+                <CustomLink to="/blogs">Blogs</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
 
         </nav>
         </div>
