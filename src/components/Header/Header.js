eaff1f9fdb2d24098db18d9632f20e3dcca1ff4d
nazibhaque137 <nazibhaque137@gmail.com>
2022-04-04 16:25:53 +0600

    nav Link added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
+        <div>
+     <h1>Smart Watch World Header</h1>
         <nav>
-            <h1>Smart Watch World</h1>
+                <Link to="/">Home</Link>
+                <Link to="/reviews">Reviews</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/blogs">Blogs</Link>
+                <Link to="/about">About</Link>
+
         </nav>
+        </div>
     );
 };
 
