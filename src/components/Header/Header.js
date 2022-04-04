36a855cfc4458496f34296342b3065eab05b724e
nazibhaque137 <nazibhaque137@gmail.com>
2022-04-04 15:43:38 +0600

    file updated

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <h1>Smart Watch World</h1>
+        </nav>
+    );
+};
+
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Smart Watch World</h2>
+        <div className="product">
+            <h2>Home</h2>
+            {/*
+            <div className="product-container">
+                <div className="product-details">
+                    <h5>The Smart Watch <br> For Smart People</br></h5>
+                    <p>With this Smart Watch you can not only get amazing ways to display time but also mind blowing features. Some of the features includes heart rate monitoring, Calorie calculator, Weather updates and so on. Don't miss the opportunity to grab this amazing piece!</p>
+                </div>
+                <div className="product-img">
+                    <img src="https://img.freepik.com/free-vector/white-smartwatch-background_1412-32.jpg?t=st=1649063699~exp=1649064299~hmac=a754171f86d539ac61c554ea79895ccbfab14c35ec35026b7609e14ef391ea11"/>
+                </div>
+            </div>
+    */}
         </div>
     );
 };
